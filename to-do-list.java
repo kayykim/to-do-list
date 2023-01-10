57bc8592873eb5fed33e1f6b5cc132e09e0baae8
kayykim <122065552+kayykim@users.noreply.github.com>
2023-01-10 12:12:00 -0500

    Added menu

--- to-do-list.java
+++ to-do-list.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
+public class toDoList{
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
+
+        // Introduction
+
+        System.out.println("This is a To-Do List!");
+        System.out.println("1: Show List");
+        System.out.println("2: Add to list");
+        System.out.println("3: Delete list");
+        System.out.println("4: Exit");
+
+        int choice;
+     
